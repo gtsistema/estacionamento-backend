5c29b67fab2442965c5db5c408a44222ff3e89f6
jean correa <jeancpcorrea@gmail.com>
2026-07-21 12:20:34 -0400

    te

--- Estac.Infra/Integration/Workers/EstacionamentoWorkersClient.cs
+++ Estac.Infra/Integration/Workers/EstacionamentoWorkersClient.cs
@@ -36,7 +36,13 @@ namespace Estac.Infra.Integration.Workers
             MovimentacaoTempoRealRequest request,
             CancellationToken cancellationToken = default)
         {
+
+
             var opts = _options.Value;
+
+            _logger.LogWarning(@$"base: {opts.BaseUrl}");
+            _logger.LogWarning(@$"opts: {opts}");
+
             if (!opts.Enabled || string.IsNullOrWhiteSpace(opts.BaseUrl))
                 return null;
 
